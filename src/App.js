26668398f744307778c4a8043b37d548d82af634
Tonkabits <tonkabits@gmail.com>
2021-10-15 17:05:20 -0400

    Initial commit from starter code React TailwindCSS Axios

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import data from './data.json';
 import Navbar from './components/Navbar';
-import CountryList from './components/CountryList';
-import CountryDetail from './components/CountryDetail';
-import countryData from './data.json'
-import axios from 'axios';
 
 export default class App extends React.Component {
 
-
-state = {
-
-  countries: [],
-  countriesAxios: [],
-  loaded: false
-
-}
-
-componentDidMount() {
-  const url = 'https://restcountries.com/v3.1/all'
-  axios.get(url).then((response) => {
-    this.setState({
-      countriesAxios: response.data,
-      loaded: true
-    })
-  }).catch(err => console.log(err))
-
-
-  this.setState({
-    countries: [...countryData]
-  })
-} 
-
-
-updateSearchTerm = event => {
-  console.log('updated')
-  this.setState({
-    searchInput: event.target.value
-  })
-
-  return this.filterFoodList()
-}
 
 
   render() {
@@ -50,31 +12,6 @@ updateSearchTerm = event => {
       <div className="App">
         <Router>
           <Navbar/>
-          <div className="w-full flex">
-            <div className="w-1/3 h-screen overflow-scroll bg-gray-50">
-            {this.state.loaded &&
-             <CountryList className=""
-             // countryData={countryData}
-            //  countryData={this.state.countries}
-             countryData={this.state.countriesAxios}
-             />
-            }
-            </div>
-            <div className="w-2/3">
-              {this.state.loaded && 
-              <Route path="/:countryCode"
-              render={(props) => 
-                <CountryDetail 
-                // countryData={countryData}
-                // countryData={this.state.countries}
-                countryData={this.state.countriesAxios}
-                {...props}
-                />
-              } 
-              />
-            }
-            </div>
-          </div>
         </Router>
       </div>
     );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
+import React, {useState, Component } from 'react'
+import { Switch } from '@headlessui/react'
 
 export default class Navbar extends Component {
+    state={
+        enabled: true,
+    }
+    
+    setEnabled = () => {
+        this.setState({
+            enabled: !this.state.enabled
+        })
+    }
+
     render() {
         return (
             <div className="w-full flex items-center text-left px-8 bg-blue-500 h-16 text-3xl text-white">
-                Wiki Countries
+                <span className="mr-4">
+                    Header
+                </span>
+                <Switch
+                    checked={this.state.enabled}
+                    onChange={this.setEnabled}
+                    className={`${this.state.enabled ? 'bg-yellow-600' : 'bg-gray-200'
+                        } relative inline-flex items-center h-5 rounded-full w-10`}
+                >
+                    <span className="sr-only">Enable notifications</span>
+                    <span
+                        className={`${this.state.enabled ? 'translate-x-6' : 'translate-x-1'
+                            } inline-block w-3 h-3 transform bg-white rounded-full`}
+                    />
+                </Switch>
+
             </div>
         )
     }
