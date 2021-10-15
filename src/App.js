165ec91d6a858e8ab1996458a14fbbac346d79b5
Tonkabits <tonkabits@gmail.com>
2021-10-15 12:35:45 -0400

    Loading data via Axios, and adjusted for countries with no borders Arr

--- src/App.js
+++ src/App.js
@@ -6,17 +6,33 @@ import Navbar from './components/Navbar';
 import CountryList from './components/CountryList';
 import CountryDetail from './components/CountryDetail';
 import countryData from './data.json'
-
+import axios from 'axios';
 
 export default class App extends React.Component {
 
 
 state = {
 
-
+  countries: [],
+  countriesAxios: [],
+  loaded: false
 
 }
 
+componentDidMount() {
+  const url = 'https://restcountries.com/v3.1/all'
+  axios.get(url).then((response) => {
+    this.setState({
+      countriesAxios: response.data,
+      loaded: true
+    })
+  }).catch(err => console.log(err))
+
+
+  this.setState({
+    countries: [...countryData]
+  })
+} 
 
 
 updateSearchTerm = event => {
@@ -36,21 +52,27 @@ updateSearchTerm = event => {
           <Navbar/>
           <div className="w-full flex">
             <div className="w-1/3 h-screen overflow-scroll bg-gray-50">
+            {this.state.loaded &&
              <CountryList className=""
-                countryData={countryData}
+             // countryData={countryData}
+            //  countryData={this.state.countries}
+             countryData={this.state.countriesAxios}
              />
+            }
             </div>
             <div className="w-2/3">
-     
+              {this.state.loaded && 
               <Route path="/:countryCode"
               render={(props) => 
                 <CountryDetail 
-                  countryData={countryData}
-                  {...props}
+                // countryData={countryData}
+                // countryData={this.state.countries}
+                countryData={this.state.countriesAxios}
+                {...props}
                 />
-              }
+              } 
               />
-       
+            }
             </div>
           </div>
         </Router>
--- src/components/CountryDetail/index.js
+++ src/components/CountryDetail/index.js
@@ -1,34 +1,92 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class CountryDetail extends Component {
     
-   test = () => {
-       console.log(this.props)
-   } 
 
-   
+    // getCountryFullName = abv => {
+    //   return  (this.props.countryData.find(country => 
+    //         abv.border === country.cca3
+         
+            
+    //     )
+    //     )
+
+    
+        
+    // }
+
+
+    findBorderingCountries = (countryDetails) => {
+        countryDetails.borders.map((border) => {
+            // let full =  this.getCountryFullName({border})
+            return <Link to={border}>
+                        {border}
+                    </Link>
+        })        
+    }
+
    render() {
        const countryCode = this.props.match.params.countryCode
        const countryDetails = this.props.countryData.find(
            (country) => country.cca3 === countryCode
        )
+
+        
+
        console.log(countryDetails)
         return (
             <div className="p-10 text-left">
                 <h2 className="text-4xl text-left font-bold mb-8">
                     {countryDetails.name.common}
                 </h2>
-                <p className="h-12 flex items-center">Capital: <span className="text-xl ml-12 font-bold">
-                    {countryDetails.capital} 
-                </span> 
-                    </p>
+                <div className="w-full flex items-center">
+                    <div className="w-1/4">
+                        <p className="h-12 flex items-center">Capital: 
+                        </p>
+                        
+                    </div>
+                    <div className="w-3/4">
+                        <span className="text-xl font-bold">
+                            {countryDetails.capital} 
+                        </span> 
+                    </div>
+                </div>
+                <hr />
+                <div className="w-full flex items-center">
+                    <div className="w-1/4">
+                        <p className="h-12 flex items-center">Area:
+                        </p>
+                        
+                    </div>
+                    <div className="w-3/4">
+                        <span className="text-xl font-bold">
+                            {countryDetails.area} </span>
+                    </div>
+                </div>
+                <hr />
+                <div className="w-full flex items-center">
+                    <div className="w-1/4">
+                        <p className="h-12 flex items-center">Borders:</p> 
+                    </div>
+                    <div className="w-3/4">
+                        <span className="text-xl font-bold">
+                            {countryDetails.borders && countryDetails.borders.length > 0  ?
+                            countryDetails.borders.map((border) => {
+                                // let full =  this.getCountryFullName({border})
+                                return  <div>
+                                <Link to={border}>
+                                {border}
+                                    </Link>
+                                </div>
+                            }): 'no borders'}  
+                            {/* {this.findBorderingCountries({countryDetails})} */}
+                            
+                        </span>
+                    </div>
+                </div>
                 <hr/>
-                <p className="h-12 flex items-center">Area: <span className="text-xl ml-12 font-bold">
-                    {countryDetails.area} </span>
-                
-                </p>
-                <hr/>
-                <p className="h-12 flex items-center">Borders:</p>
+              
             </div>
         )
     }
